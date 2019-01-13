021e7ded12d2fb5aa3f510fe4a2640b4867673e6
Torres, Vasco <Vasco.Torres@adtalem.com>
2019-01-13 23:59:02 +0000

    Add method to record new gesture from right hand

---
+++ Assets/_Scripts/Data/Gesture.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Gesture{
+    public float Duration;
+}
+
+public class PositionGesture : Gesture{
+    public Vector3[] Positions;
+}
+
+// time needs to be fixed
+/*
+gesture = 3s
+
+250 frames
+
+
+3 - 250
+0.34 - X
+
+50fps
+
+17/50 = 0.34s*/
+
+/*
+210 frames at 60fps = 3.5s
+
+210 values
+
+-------
+
+300 frames at 50fps = 6s
+
+300 values
+
+*/
+
+/*
+20 fps
+0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 6 6.5 7 7.5 8 8.5 9 9.5 10
+
+10 fps
+1 2 3 4 5 6 7 8 9 10
+
+5 fps
+1 2.5 5 7.5 10
+
+
+0.75s
+
+20 * 3/4 = 15 => 7.5
+
+10 * 3/4 = 7.5 => 7.5
+
+5 * 3/4 = 3.75 = > 6.875
+*/
---
+++ Assets/_Scripts/RecordGesture.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using VRTK;
+
+public class RecordGesture : MonoBehaviour{
+    public VRTK_ControllerEvents VrtkControllerEvents;
+    public VRTK_ControllerEvents.ButtonAlias RecordingButton;
+    private Coroutine _recordingCoroutine;
+    private bool _recording;
+
+    private void Start(){
+        VrtkControllerEvents.SubscribeToButtonAliasEvent(RecordingButton, true, StartRecording);
+        VrtkControllerEvents.SubscribeToButtonAliasEvent(RecordingButton, false, StopRecording);
+    }
+
+    private void StopRecording(object sender, ControllerInteractionEventArgs e){
+        _recording = false;
+    }
+
+    private void StartRecording(object sender, ControllerInteractionEventArgs e){
+        if(_recordingCoroutine != null)
+            StopCoroutine(_recordingCoroutine);
+        _recording = true;
+        _recordingCoroutine = StartCoroutine(RecordGestureCoroutine());
+    }
+
+    private IEnumerator RecordGestureCoroutine(){
+        PositionGesture newGesture = new PositionGesture();
+        Transform rightHand = VRTK_DeviceFinder.GetControllerRightHand().transform;
+        List<Vector3> positions = new List<Vector3>();
+        float t = Time.realtimeSinceStartup;
+        while (_recording){
+            positions.Add(rightHand.position);
+            yield return null;    
+        }
+        //Debug.Log(positions.Count + " positions recorded");
+        //Debug.Log("In " + (Time.realtimeSinceStartup - t) + " seconds");
+        newGesture.Duration = Time.realtimeSinceStartup - t;
+        newGesture.Positions = positions.ToArray();
+        string jsonData = JsonUtility.ToJson(newGesture);
+        File.WriteAllText(Application.dataPath + "\\NewGesture.json", jsonData);
+    }
+}
